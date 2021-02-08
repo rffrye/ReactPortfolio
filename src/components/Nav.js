9369e90ee4bfa616b6977fb6749ffbf9cf839a14
RFFRYE <rffrye@users.noreply.github.com>
2021-02-07 23:08:44 -0500

    changed Nav.js to try and bug fix routing issue.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,15 +3,15 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav"
 import Logo from "../imgs/Logo.png"
 
-const NavBar = ()=> {
+function NavBar() {
 return (
         <Navbar bg="light" variant="light">
             <Navbar.Brand href="/ReactPortfolio"> <img src={Logo} width="70px"/></Navbar.Brand>
-            <Nav className="mr-auto">
-            <Nav.Link href="/ReactPortfolio">Home</Nav.Link>
-            <Nav.Link href="/ReactPortfolio/portfolio">Portfolio</Nav.Link>
-            <Nav.Link href="/ReactPortfolio/contact">Contact</Nav.Link>
-            </Nav>
+                <Nav className="mr-auto">
+                    <Nav.Link href="/ReactPortfolio">Home</Nav.Link>
+                    <Nav.Link href="/ReactPortfolio/portfolio">Portfolio</Nav.Link>
+                    <Nav.Link href="/ReactPortfolio/contact">Contact</Nav.Link>
+                </Nav>
         </Navbar>
     );
 };
